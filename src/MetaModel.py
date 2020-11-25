508d1a9f18b6269b4e1a44d6dc298e843d5caa59
Carter Blum <blumx116@umn.edu>
2020-11-25 13:26:45 -0600

    add @abstractmethod tags to MetaModel.py

--- src/MetaModel.py
+++ src/MetaModel.py
@@ -1,24 +1,29 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import Optional
 
 import torch.nn as nn
 
 
 class MetaModel(ABC):
+    @abstractmethod
     def add_tasks(self,
             n_tasks: int) -> None:
         pass
 
+    @abstractmethod
     def set_task(self,
             task_idx: int) -> None:
         pass
 
+    @abstractmethod
     def get_task(self) -> Optional[int]:
         pass
 
+    @abstractmethod
     def shared_parameters(self) -> nn.ParameterList:
         pass
 
+    @abstractmethod
     def task_parameters_for(self,
             task_idx: int) -> nn.ParameterList:
         pass
